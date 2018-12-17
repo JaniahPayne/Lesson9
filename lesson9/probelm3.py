54c056305d2c43f556c77329627258ba37c4dae1
JaniahPayne <44148033+JaniahPayne@users.noreply.github.com>
2018-12-17 12:53:59 -0500

    Add files via upload

---
+++ lesson9/probelm3.py
@@ -0,0 +1,10 @@
+print('_' *60)
+print('Alien Greeter Bot:')
+print()
+planet = input('What planet are you from?')
+if planet == 'Earth':
+	print('Damnnnnnn, your face is so ugly i thought you were an alien. Anyway... have a good day!')
+else:
+	print('Welcome to Earth, please do not kill us!')
+	
+print('_'*60)
---
+++ lesson9/problem2.py
@@ -0,0 +1,11 @@
+print('_' *60)
+print('Locker bot:')
+print()
+password = input('What is the password?')
+if password == 'Open Sesame':
+	print('Password is vaild!')
+	print('Locker opening!')
+else:
+	print('Password is invaild..enabling laser defense system, please stand down.')
+	print('Pew Pew Pew!')
+print('_'*60)
